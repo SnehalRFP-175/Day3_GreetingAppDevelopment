9b5793c85f8f8ae99941e6907a5b4eb4c5f64b4d
Snehal <nanavaresnehal115@gmail.com>
2022-10-17 12:57:29 +0530

    ADDED[Snehal] UC4- Saving Message to the Database

--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -1,12 +1,34 @@
 package com.bridgelabz.greetingapp.service;
 
+import com.bridgelabz.greetingapp.entity.Greeting;
 import com.bridgelabz.greetingapp.entity.User;
+import com.bridgelabz.greetingapp.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class GreetingService implements IGreetingService{
 
-    public String getGreetingMessage(User user){
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    public String getGreetingMessage(){
         return "Hello World!";
+    }
+
+    @Override
+    public String getGreetingMessage(User user) {
+        String name = user.toString().isEmpty() ? "Hello world " : user.toString();
+        return String.format(template,name);
+    }
+    @Override
+    public Greeting addGreetingMessage(User user) {
+        String name = user.toString().isEmpty() ? "Hello world " : user.toString();
+        return greetingRepository.save(new Greeting(counter.incrementAndGet(),String.format(template,name)));
     }
 }

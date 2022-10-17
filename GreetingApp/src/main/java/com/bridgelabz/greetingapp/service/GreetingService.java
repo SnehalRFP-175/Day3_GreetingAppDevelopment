9bd4e34a61aab6d65d901d68ed28e3f999cb8a23
Snehal <nanavaresnehal115@gmail.com>
2022-10-17 12:44:19 +0530

    ADDED[Snehal] UC2- Service Layer to get Message with User Name

--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -1,11 +1,12 @@
 package com.bridgelabz.greetingapp.service;
 
+import com.bridgelabz.greetingapp.entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService implements IGreetingService{
 
-    public String getGreetingMessage(){
+    public String getGreetingMessage(User user){
         return "Hello World!";
     }
 }
